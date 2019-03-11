a37c282b8dda73997acb4fd89ecf44f39cb94707
ln2r <scorch_62@live.com>
2019-03-12 00:10:21 +0700

    added classes role creaction command

---
+++ commands/guild/create.js
@@ -0,0 +1,46 @@
+const { Command } = require('discord.js-commando');
+const core = require('../../core.js');
+
+module.exports = class CreateChCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'create',
+            aliases: ['make', 'setup'],
+            group: 'guild',
+            memberName: 'create',
+            description: 'Create classes role',
+            examples: ['create'],
+            guildOnly: true,
+            userPermission: ['ADMINISTRATOR', 'MANAGE_CHANNELS', 'MANAGE_GUILD', 'MANAGE_MESSAGES', 'MANAGE_NICKNAMES', 'MANAGE_ROLES'],
+        });
+    }
+
+    async run(msg) {
+        let msgData = '';
+        let guildSettingsData = await core.mongoGetData('guilds', {guild: msg.guild.id});
+        let classList = ['gunslinger', 'blade dancer', 'destroyer', 'summoner', 'kung fu master', 'assassin', 'force master', 'warlock', 'blade master', 'soul fighter', 'warden'];
+
+        // checking if the guild already done the setup or not
+        if(guildSettingsData[0].roles_setup == undefined){
+            for(let i = 0; i < classList.length; i ++){
+                // checking if the current class role existed or not
+                if((msg.guild.roles.find(role => role.name == classList[i])) == null){
+                    // creating the roles
+                    msg.guild.createRole({
+                        'name': classList[i],
+                        'hoist': true
+                    })
+                    console.debug('[soyun] [role-setup] '+classList[i]+' role created @ '+msg.guild.name);
+                }                
+            }
+
+            this.client.emit('guildRolesSetup', msg.guild.id, true); // updating the database
+
+            msgData = 'Necessary classes roles created, go to `Server Settings > Roles` to check and configure it'
+        }else{
+            msgData = 'Classes roles already created, go to `Server Settings > Roles` to check and configure it'
+        }
+
+        return msg.say(msgData);
+    }
+};
