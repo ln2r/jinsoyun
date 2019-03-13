158fd3eba670793a270935604217456eced17ac7
ln2r <scorch_62@live.com>
2019-03-13 19:18:41 +0700

    added shackled isle closest time checker

---
+++ commands/bns/shackledisle.js
@@ -0,0 +1,44 @@
+const { Command } = require('discord.js-commando');
+const dateformat = require('dateformat');
+
+const core = require('../../core.js');
+
+module.exports = class ShackledIsleCommand extends Command {
+    constructor(client) {
+        super(client, {
+            name: 'shackledisle',
+            aliases: ['br', 'shackled', 'isle', 'battleroyale'],
+            group: 'bns',
+            memberName: 'shackledisle',
+            description: 'Get Shackled Isle access time',
+            examples: ['shackledisle'],
+        });    
+    }
+
+    async run(msg) {
+        msg.channel.startTyping();
+
+        let timeData = await core.mongoGetData('challenges', {});
+            timeData = timeData[0].shackled_isle.time;
+
+        let brModeClosestTime = core.getTimeDifference(timeData);
+
+        //console.debug('[soyun] [shackled isle] ['+msg.guild.name+'] time diffence data: '+JSON.stringify(brModeClosestTime, null, '\t'));
+
+        msg.channel.stopTyping();
+
+        let embedData = {
+            'embed': {
+                'author': {
+                    'name': 'Battle Royale Shackled Isle ('+timeData[brModeClosestTime.time_index]+':00 UTC)',
+                },
+                'color': 8388736,
+                'footer': {
+                    'text': 'Shackled Isle - Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC'
+                },
+                'description': 'Available in '+brModeClosestTime.time_difference_data[0]+' hour(s) and '+brModeClosestTime.time_difference_data[1]+' minute(s)',
+            }
+        }
+        return msg.say(embedData);
+    }
+};
