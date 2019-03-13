b2d406ee727f11fa2a23be768f2199f84449fc40
ln2r <scorch_62@live.com>
2019-03-13 19:19:35 +0700

    changed time difference checker to use function instead

--- commands/bns/koldrak.js
+++ commands/bns/koldrak.js
@@ -21,65 +21,22 @@ module.exports = class KoldrakCommand extends Command {
         let timeData = await core.mongoGetData('challenges', {});
             timeData = timeData[0].koldrak.time;
 
-        let koldrakNextHourMax = 24; // maximum hour distance
-
-        let now = new Date();
-        let timeNow = new Date(0, 0, 0, now.getUTCHours(), now.getMinutes());
-
-        let closestTime;
-        let koldrakNextTimeData;
-
-        console.debug('[soyun] [koldrak] now: '+timeNow);
-
-        for(let i = 0; i < timeData.length; i++){
-            let koldrakAccess = new Date(0, 0, 0, timeData[i], 0);
-
-            let timeRemaining = (koldrakAccess - timeNow);
-            //console.debug('[soyun] [koldrak] current: '+koldrakAccess);
-            //console.debug('[soyun] [koldrak] remain: '+timeRemaining);
-
-            // formatting the data
-            let koldrakNextHour = Math.abs(Math.floor(timeRemaining / 1000 / 60 / 60));
-            // use extra variable so 'timerRemaining' variable remain unchanged
-            let koldrakNextHourRaw = timeRemaining - (koldrakNextHour * 1000 * 60 *60);
-                
-            let koldrakNextMinute = Math.abs(Math.floor(koldrakNextHourRaw / 1000 / 60));
-
-            //console.debug('[soyun] [koldrak] left: '+koldrakNextHour+'h '+koldrakNextMinute+'m')
+        let koldrakClosestTime = core.getTimeDifference(timeData);
+        //console.debug('[soyun] [koldrak\'s lair] ['+msg.guild.name+'] time diffence data: '+JSON.stringify(koldrakClosestTime, null, '\t'));
     
-            // checking if current time is smaller than last one or not
-            if(koldrakNextHour <= koldrakNextHourMax){
-                koldrakNextHourMax = koldrakNextHour;
-                closestTime = i;
-                // storing the formatted data into an array
-                koldrakNextTimeData = [koldrakNextHour, koldrakNextMinute];
-            }
-            /*
-            if(koldrakTimeHours <= koldrakTimeLeft){
-									if(koldrakTimeHours >= 0){
-										koldrakTimeLeft = koldrakTimeHours;
-									}
-								}
-            */
-        }    
-        console.debug('[soyun] [koldrak] selected: '+new Date(0, 0, 0, timeData[closestTime], 0));
-        //console.debug('[soyun] [koldrak] time data '+timeData);
-
-        console.debug('[soyun] [koldrak] time left: '+koldrakNextTimeData[0]+' hours, '+koldrakNextTimeData[1]+' minutes');
-
         msg.channel.stopTyping();
 
         let embedData = {
             'embed': {
                 'author': {
-                    'name': 'Epic Challenge - Koldrak\'s Lair ('+timeData[closestTime]+':00 UTC)',
+                    'name': 'Epic Challenge - Koldrak\'s Lair ('+timeData[koldrakClosestTime.time_index]+':00 UTC)',
                 },
                 'color': 8388736,
                 'footer': {
                     'icon_url': 'https://cdn.discordapp.com/emojis/463569669584977932.png?v=1',
                     'text': 'Koldrak\'s Lair - Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC'
                 },
-                'description': 'Available in '+koldrakNextTimeData[0]+' hour(s) and '+koldrakNextTimeData[1]+' minute(s)',
+                'description': 'Available in '+koldrakClosestTime.time_difference_data[0]+' hour(s) and '+koldrakClosestTime.time_difference_data[0]+' minute(s)',
             }
 
         }
