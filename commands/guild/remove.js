9f78cdc18c1d9c490bd73c7ff5bee9fda93cd018
ln2r <scorch_62@live.com>
2019-04-13 22:53:08 +0700

    fixed "Cannot read property 'id' of null"

--- commands/guild/remove.js
+++ commands/guild/remove.js
@@ -43,10 +43,13 @@ module.exports = class RemoveCustomRoleCommand extends Command {
         if(customRoles.length != 0){
             // checking if the role valid
             let guildRolesData = msg.guild.roles.find(role => role.name == args);
-            let userRolesData = msg.guild.members.get(msg.author.id).roles.find(role => role.id == guildRolesData.id);
-            console.debug('[soyun] [custom-roles-remove] role data: '+guildRolesData);
-            console.debug('[soyun] [custom-roles-remove] user role data: '+userRolesData);
-            console.debug('[soyun] [custom-roles-remove] guild custom roles data:'+guildCustomRolesData);
+            if(guildRolesData != undefined){
+                var userRolesData = msg.guild.members.get(msg.author.id).roles.find(role => role.id == guildRolesData.id);
+            }
+            
+            //console.debug('[soyun] [custom-roles-remove] role data: '+guildRolesData);
+            //console.debug('[soyun] [custom-roles-remove] user role data: '+userRolesData);
+            //console.debug('[soyun] [custom-roles-remove] guild custom roles data:'+guildCustomRolesData);
 
             // checking if the args is valid
             for(let i=0; i<guildCustomRolesData.length; i++){
@@ -57,7 +60,7 @@ module.exports = class RemoveCustomRoleCommand extends Command {
 
             if(guildRolesData != null && argsValid){
                 // checking if user have role
-                if(userRolesData != null){
+                if(userRolesData != null && userRolesData != undefined){
                     // checking if the bot have the permission
                     if(msg.channel.permissionsFor(this.client.user).has("MANAGE_ROLES", false)){
                         // removing the role
