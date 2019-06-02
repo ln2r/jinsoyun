4e1c8a59ced8251a7fd0af8dcb4c983bcd55b39c
ln2r <scorch_62@live.com>
2019-06-02 17:17:25 +0700

    added handler for reaction-role once

--- index.js
+++ index.js
@@ -144,6 +144,8 @@ clientDiscord
       const { d: data } = event;
       const user = clientDiscord.users.get(data.user_id);
 
+      // console.debug("user id: "+data.user_id);
+
       // check if it's the bot
       if(data.user_id === clientDiscord.user.id) return;
       
@@ -164,14 +166,14 @@ clientDiscord
               found = true;
             };
           };
-          console.debug("found: "+found+" @ "+messageIndex+" message id: "+data.message_id);
+          // console.debug("found: "+found+" @ "+messageIndex+" message id: "+data.message_id);
           if(found){
             // checking the message
             if(data.message_id === guildReactionRoleData[messageIndex].id){
               const message = await channel.fetchMessage(data.message_id);
               const member = message.guild.members.get(user.id);
 
-              console.debug("selected reaction: "+data.emoji.name+":"+data.emoji.id);
+              // console.debug("selected reaction: "+data.emoji.name+":"+data.emoji.id);
 
               // checking the emoji and getting the index
               if(guildReactionRoleData[messageIndex].reactions){
@@ -182,21 +184,36 @@ clientDiscord
                   emojiData = data.emoji.name;
                 };
 
-                console.debug("reaction isn't empty");
-
                 // console.debug("emoji data: "+emojiData);
                 // checking the emoji and getting the role id
+                let reactionFound = false;
+                let reactionIndex;
                 for(let i=0; i<guildReactionRoleData[messageIndex].reactions.length; i++){
                   if(guildReactionRoleData[messageIndex].reactions[i].emoji === emojiData){
-                    console.debug("role to be added with id: "+guildReactionRoleData[messageIndex].reactions[i].role);
-                    // adding or removing the role
+                    reactionFound = true;
+                    reactionIndex = i;
+                  };
+                };
+
+                if(reactionFound){
+                  // checking if once
+                  if(guildReactionRoleData[messageIndex].reactions[reactionIndex].once){
+                    member.addRole(guildReactionRoleData[messageIndex].reactions[reactionIndex].role);
+                    // removing the reaction
+                    channel.fetchMessage(guildReactionRoleData[messageIndex].id).then(message => {
+                      let filtered = message.reactions.filter(reaction => reaction.emoji.name === data.emoji.name);
+
+                      filtered.forEach(reaction => reaction.remove(user.id));
+                    });
+                  }else{
+                    // check if the event add or remove
                     if(event.t === "MESSAGE_REACTION_ADD"){
                       member.addRole(guildReactionRoleData[messageIndex].reactions[i].role);
                     }else{
                       member.removeRole(guildReactionRoleData[messageIndex].reactions[i].role)
                     };
-                  };
-                };
+                  }
+                }
               };                
             };
           };
