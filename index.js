c2f7de31fb1732c64bf6f749b569d4de9c672b1a
ln2r <scorch_62@live.com>
2019-06-04 02:52:13 +0700

    added maintenace mode, permission check on twitter

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const dotenv = require('dotenv').config();
 
-const {CommandoClient} = require('discord.js-commando');
+const { CommandoClient } = require('discord.js-commando');
 const Twitter = require('twitter');
 const MongoClient = require('mongodb').MongoClient;
 const MongoDBProvider = require('./commando-provider-mongo');
@@ -9,6 +9,9 @@ const ontime = require('ontime');
 const dateformat = require('dateformat');
 
 const {mongoGetData, sendResetNotification, mongoItemDataUpdate, sendBotReport, sendBotStats} = require('./core');
+
+// Maintenance mode
+const maintenanceMode = process.env.bot_maintenance_mode;
 
 // Discord.js Commando scripts start here
 const clientDiscord = new CommandoClient({
@@ -61,76 +64,84 @@ clientDiscord
           name: '@Jinsoyun help',
           type: 'LISTENING',
         },
-      }
-      )
-          .catch((error) => {
-            console.error;
-            sendBotReport(error, 'onReady-soyun', 'error');
-          });
+      }).catch((error) => {
+        console.error;
+        sendBotReport(error, 'onReady-soyun', 'error');
+      });
+
+      if(maintenanceMode){
+        console.log("[soyun] [system] maintenance mode is enabled, only commands will run normally");
+      };      
     })
     .on('guildMemberAdd', async (member) => {
       let guildSettingData = await mongoGetData('guilds', {guild: member.guild.id});
       guildSettingData = guildSettingData[0];
 
       let memberGate = '';
-      if (guildSettingData !== undefined) {
+      if (guildSettingData) {
         memberGate = guildSettingData.settings.member_gate;
-      }
+        // console.debug("[soyun] [gate] ["+member.guild.name+"] memberGate value: "+memberGate);
+        // checking if the guild have the channel and the message set
+        if ((memberGate.channel_id !== '' || memberGate.channel_id) && (memberGate.message !== "" || memberGate.message)) {
+          member.guild.channels.find((ch) => ch.id === memberGate.channel_id).send(
+            'Hi <@'+member.user.id+'>! Welcome to ***'+member.guild.name+'***!\n\n'+
 
-      // console.debug("[soyun] [gate] ["+member.guild.name+"] memberGate value: "+memberGate);
+            'Before I give you access to the rest of the server, I need to know your character\'s name, To do that, please use following command with your information in it\n'+
+            '`@Jinsoyun join character name`\n'+
+            '**Example**:\n'+
+            '`@Jinsoyun join jinsoyun `\n\n'+
 
-      // checking if the guild have the channel and the message set
-      if ((memberGate.channel_id !== '' || memberGate.channel_id !== undefined) && (memberGate.message !== "" || memberGate.message !== undefined)) {
-        member.guild.channels.find((ch) => ch.id === memberGate.channel_id).send(
-          'Hi <@'+member.user.id+'>! Welcome to ***'+member.guild.name+'***!\n\n'+
-
-          'Before I give you access to the rest of the server, I need to know your character\'s name, To do that, please use following command with your information in it\n'+
-          '`@Jinsoyun join character name`\n'+
-          '**Example**:\n'+
-          '`@Jinsoyun join jinsoyun `\n\n'+
-
-          'if you need any assistance you can mention or DM available admins, thank you ❤'
-        );
-      }
+            'if you need any assistance you can mention or DM available admins, thank you ❤'
+          );
+        };
+      };
     })
     .on('commandRun', async () => {
-      await sendBotStats(Date.now());
+      if(maintenanceMode){
+        console.log("[soyun] [stats] command counter disabled");
+      }else{
+        await sendBotStats(Date.now());
+      };
     })
     .on('commandError', (error, command, message) => {
-      // sending the error report to the database
-      sendBotReport(command.name+': '+command.message, error.name+'-'+message.guild.name, 'error');
-      console.error('[soyun] ['+error.name+'] '+command.name+': '+command.message);
+      if(maintenanceMode){
+        console.log("[soyun] [error-report] error reporting disabled");
+      }else{
+        // sending the error report to the database
+        sendBotReport(command.name+': '+command.message, error.name+'-'+message.guild.name, 'error');
+        console.error('[soyun] ['+error.name+'] '+command.name+': '+command.message);
 
-      // dm bot owner for the error
-      let found = 0;
-      clientDiscord.guilds.map(function(guild) { // looking for the guild owner data (username and discriminator)
-        guild.members.map((member) => {
-          if (found === 0) {
-            if (member.id === message.guild.ownerID) {
-              found = 1;
+        // dm bot owner for the error
+        let found = 0;
+        clientDiscord.guilds.map(function(guild) { // looking for the guild owner data (username and discriminator)
+          guild.members.map((member) => {
+            if (found === 0) {
+              if (member.id === message.guild.ownerID) {
+                found = 1;
 
-              for (let i=0; i < clientDiscord.owners.length; i++) {
-                clientDiscord.owners[i].send(
-                    'Error Occured on `'+error.name+'`'+
-                                '\n__Details__:'+
-                                '\n**Time**: '+dateformat(Date.now(), 'dddd, dS mmmm yyyy, h:MM:ss TT')+
-                                '\n**Location**: '+message.guild.name+
-                                '\n**Guild Owner**: '+member.user.username+'#'+member.user.discriminator+
-                                '\n**Content**: `'+message.content+'`'+
-                                '\n**Message**:\n'+command.name+': '+command.message
-                ).then(
-                    function(message) {
-                      message.react('✅');
-                      message.react('❎');
-                    }
-                ).catch((err) => {
-                  sendBotReport(err, 'errorDM-soyun', 'error');
-                });
+                for (let i=0; i < clientDiscord.owners.length; i++) {
+                  clientDiscord.owners[i].send(
+                      'Error Occured on `'+error.name+'`'+
+                                  '\n__Details__:'+
+                                  '\n**Time**: '+dateformat(Date.now(), 'dddd, dS mmmm yyyy, h:MM:ss TT')+
+                                  '\n**Location**: '+message.guild.name+
+                                  '\n**Guild Owner**: '+member.user.username+'#'+member.user.discriminator+
+                                  '\n**Content**: `'+message.content+'`'+
+                                  '\n**Message**:\n'+command.name+': '+command.message
+                  ).then(
+                      function(message) {
+                        message.react('✅');
+                        message.react('❎');
+                      }
+                  ).catch((err) => {
+                    sendBotReport(err, 'errorDM-soyun', 'error');
+                  });
+                }
               }
             }
-          }
+          });
         });
-      });
+      };
     })
     // event handling for reactions
     .on('raw', async event => {
@@ -231,136 +242,145 @@ clientDiscord.setProvider(
 // Discord.js Commando scripts end here
 
 // Twitter stream scripts start here
-const clientTwitter = new Twitter({
-  consumer_key: process.env.twitter_consumer_key,
-  consumer_secret: process.env.twitter_consumer_secret,
-  access_token_key: process.env.twitter_access_token_key,
-  access_token_secret: process.env.twitter_access_token_secret,
-});
+if(maintenanceMode){
+  console.log("[soyun] [twitter] twitter is disabled");
+}else{
+  const clientTwitter = new Twitter({
+    consumer_key: process.env.twitter_consumer_key,
+    consumer_secret: process.env.twitter_consumer_secret,
+    access_token_key: process.env.twitter_access_token_key,
+    access_token_secret: process.env.twitter_access_token_secret,
+  });
 
-clientTwitter.stream('statuses/filter', {follow: '3521186773, 819625154'}, async function(stream) {
-  const twitterAPIData = await mongoGetData('apis', {name: 'Twitter'});
-  const twitterTrackedUser = twitterAPIData[0].stream_screenname;
+  clientTwitter.stream('statuses/filter', {follow: '3521186773, 819625154'}, async function(stream) {
+    const twitterAPIData = await mongoGetData('apis', {name: 'Twitter'});
+    const twitterTrackedUser = twitterAPIData[0].stream_screenname;
+    let twitterUserValid = false;
 
-  let twitterUserValid = false;
+    stream.on('data', function(tweet) {
+      let payloadStatus = 'rejected';
 
-  stream.on('data', function(tweet) {
-    let payloadStatus = 'rejected';
-
-    // checking if it's valid account
-    for (let i=0; i<twitterTrackedUser.length; i++) {
-      if (tweet.user.screen_name === twitterTrackedUser[i]) {
-        twitterUserValid = true;
+      // checking if it's valid account
+      for (let i=0; i<twitterTrackedUser.length; i++) {
+        if (tweet.user.screen_name === twitterTrackedUser[i]) {
+          twitterUserValid = true;
+        }
       }
-    }
 
-    if (twitterUserValid) {
-      const twtFilter = tweet.text.toString().substring(0).split(' ');
+      if (twitterUserValid) {
+        const twtFilter = tweet.text.toString().substring(0).split(' ');
 
-      // Filtering the "RT" and "mention" stuff
-      if (twtFilter[0] === 'RT' || twtFilter[0].charAt(0) === '@') {
-        payloadStatus = 'rejected';
-      } else {
-        if (tweet.extended_tweet) {
-          twtText = tweet.extended_tweet.full_text.toString().replace('&amp;', '&');
+        // Filtering the "RT" and "mention" stuff
+        if (twtFilter[0] === 'RT' || twtFilter[0].charAt(0) === '@') {
+          payloadStatus = 'rejected';
         } else {
-          twtText = tweet.text.toString().replace('&amp;', '&');
-        }
-
-        if (tweet.is_quote_status) {
-          twtText = twtText+' RT @'+tweet.quoted_status.user.screen_name+' '+(tweet.quoted_status.text.toString().replace('&amp;', '&'));
-        }
-
-        payloadStatus = 'received';
-
-        // Making the color different for different user
-        if (tweet.user.screen_name === twitterTrackedUser[0]) {
-          twtColor = 16753920;
-        } else {
-          twtColor = 1879160;
-        }
-
-        // getting image if there's any
-        if(tweet.entities.media){
-          twtImage = tweet.entities.media[0].media_url;
-        }else{
-          twtImage = "";
-        }
-
-        const embedData = {
-          'embed': {
-            'title': tweet.user.name,
-            'url': 'https://twitter.com/'+tweet.user.screen_name,
-            'description': twtText,
-            'image': {
-              'url': twtImage
-            },
-            'color': twtColor,
-            'timestamp': new Date(),
-            'footer': {
-              'text': tweet.user.name,
-              'icon_url': tweet.user.profile_image_url,
-            },
-          },
-        };
-
-        // sending the tweet
-        clientDiscord.guilds.map(async function(guild) {
-          // console.debug("[soyun] [tweet] guild list: "+guild.id+"("+guild.name+")");
-
-          // getting guild setting data
-          let guildSettingData = await mongoGetData('guilds', {guild: guild.id});
-          guildSettingData = guildSettingData[0];
-          // console.debug("[soyun] [tweet] guild setting data: "+JSON.stringify(guildSettingData, null, "\t"));
-
-          let twitterChannel = '';
-          if (guildSettingData !== undefined) {
-            twitterChannel = guildSettingData.settings.twitter;
+          if (tweet.extended_tweet) {
+            twtText = tweet.extended_tweet.full_text.toString().replace('&amp;', '&');
+          } else {
+            twtText = tweet.text.toString().replace('&amp;', '&');
           }
 
-          let found = 0;
-          guild.channels.map((ch) => {
-            if (found === 0) {
-              if (ch.id === twitterChannel && twitterChannel !== '' && twitterChannel !== 'disable') {
-                found = 1;
-                ch.send(embedData);
-              }
-            }
-          });
-        });
-        console.log('[soyun] [twitter] '+tweet.user.name+'\'s tweet sent');
-      }
-    }
-    console.log('[soyun] [twitter] Twitter stream activity detected, status: '+payloadStatus);
-  });
+          if (tweet.is_quote_status) {
+            twtText = twtText+' RT @'+tweet.quoted_status.user.screen_name+' '+(tweet.quoted_status.text.toString().replace('&amp;', '&'));
+          }
 
-  stream.on('error', function(error) {
-    sendBotReport(error, 'twitter-soyun', 'error');
-    console.error(error);
+          payloadStatus = 'received';
+
+          // Making the color different for different user
+          if (tweet.user.screen_name === twitterTrackedUser[0]) {
+            twtColor = 16753920;
+          } else {
+            twtColor = 1879160;
+          }
+
+          // getting image if there's any
+          if(tweet.entities.media){
+            twtThumbnail = tweet.entities.media[0].media_url;
+          }else{
+            twtThumbnail = "";
+          }
+
+          const embedData = {
+            'embed': {
+              'title': tweet.user.name,
+              'url': 'https://twitter.com/'+tweet.user.screen_name,
+              'description': twtText,
+              'image': {
+                'url': twtThumbnail
+              },
+              'color': twtColor,
+              'timestamp': new Date(),
+              'footer': {
+                'text': tweet.user.name,
+                'icon_url': tweet.user.profile_image_url,
+              },
+            },
+          };
+
+          // sending the tweet
+          clientDiscord.guilds.map(async function(guild) {
+            // console.debug("[soyun] [tweet] guild list: "+guild.id+"("+guild.name+")");
+
+            // getting guild setting data
+            let guildSettingData = await mongoGetData('guilds', {guild: guild.id});
+            guildSettingData = guildSettingData[0];
+            // console.debug("[soyun] [tweet] guild setting data: "+JSON.stringify(guildSettingData, null, "\t"));
+
+            let twitterChannel = '';
+            if (guildSettingData !== undefined) {
+              twitterChannel = guildSettingData.settings.twitter;
+            }
+
+            let found = 0;
+            guild.channels.map((ch) => {
+              if (found === 0) {
+                if (ch.id === twitterChannel && twitterChannel !== '' && twitterChannel !== 'disable') {
+                  found = 1;
+                  if(ch.permissionsFor(clientDiscord.user).has('EMBED_LINKS', 'SEND_MESSAGES', 'VIEW_CHANNEL')){
+                    ch.send(embedData);
+                  };
+                };
+              };
+            });
+          });
+          console.log('[soyun] [twitter] '+tweet.user.name+'\'s tweet sent');
+        }
+      }
+      console.log('[soyun] [twitter] Twitter stream activity detected, status: '+payloadStatus);
+    });
+
+    stream.on('error', function(error) {
+      sendBotReport(error, 'twitter-soyun', 'error');
+      console.error(error);
+    });
   });
-});
+}
 // Twitter stream script end here
 
 // Automation scripts start here
 // Quest reset notification
-ontime({
-  cycle: ['12:00:00'],
-  utc: true,
-}, async function(reset) {
-  sendResetNotification(clientDiscord.guilds);
-  reset.done();
-  return;
-}
-);
+if(maintenanceMode){
+  console.log("[soyun] [automation] system automation is disabled");
+}else{
+  ontime({
+    cycle: ['12:00:00'],
+    utc: true,
+  }, async function(reset) {
+    sendResetNotification(clientDiscord.guilds);
+    reset.done();
+    return;
+  }
+  );
 
-// Item data update
-ontime({
-  cycle: ['00:02'],
-  utc: true,
-}, function(itemUpdate) {
-  mongoItemDataUpdate();
+  // Item data update
+  ontime({
+    cycle: ['00:02'],
+    utc: true,
+  }, function(itemUpdate) {
+    mongoItemDataUpdate();
 
-  itemUpdate.done();
-  return;
-}
-);
+    itemUpdate.done();
+    return;
+  }
+  );
+};
