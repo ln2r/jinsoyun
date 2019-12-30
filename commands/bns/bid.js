e819da253e5f5ab662063d03f155e69f06f453cb
ln2r <scorch_62@live.com>
2019-12-30 19:40:49 +0700

    update: market data rework

--- commands/bns/bid.js
+++ commands/bns/bid.js
@@ -1,6 +1,5 @@
 const { Command } = require("discord.js-commando");
 const { mongoGetData, setCurrencyFormat, getGlobalSettings } = require("../../core");
-const dateformat = require("dateformat");
 
 module.exports = class RemoveCustomRoleCommand extends Command {
     constructor(client) {
@@ -30,6 +29,10 @@ module.exports = class RemoveCustomRoleCommand extends Command {
     async run(msg, {playerCount, itemPrice}) {
         msg.channel.startTyping();
 
+        const start = Date.now();
+        let end;
+        let serveTime;
+
         // checking if the command disabled or not
         let globalSettings = await getGlobalSettings("bid");
         if(!globalSettings.status){
@@ -42,29 +45,42 @@ module.exports = class RemoveCustomRoleCommand extends Command {
         playerCount = playerCount.toString().replace(/[<>]/g, ""); 
         itemPrice = itemPrice.replace(/[<>]/g, "");
 
-        let dataLastUpdate = Date.now();
         let itemName = itemPrice;
+        let invalidItem = false;
+        let bidData;
         
+        // getting item price from market
         if(isNaN(itemPrice)){
             let regx = new RegExp("(?:^|\W)"+itemPrice+"+(?:$|\W)", "ig");
             let dbSearchQuery = {"name": regx};
-            let itemData = await mongoGetData("items", dbSearchQuery);
+            let itemData = await mongoGetData("items_", dbSearchQuery);       
 
-            itemPrice = itemData[0].market[0].priceEach;
-            dataLastUpdate = itemData[0].market[0].updated;
-            itemName = itemData[0].name;
+            if(itemData.length === 0){
+                invalidItem = true;
+            }else{
+                let marketData = await mongoGetData("market", {id: itemData[0].id});
+
+                itemPrice = marketData[0].priceEach;
+                itemName = itemData[0].name;
+            } 
         }
 
         // smart bid algorithm thanks to BnSTools - https://bnstools.info/
 
-        let selfBid = Math.floor(1 * itemPrice * ((playerCount - 1)/playerCount));
-        let marketBid = Math.floor((1 * itemPrice * ((playerCount - 1)/playerCount)) * (1 - .05 * 1));
+        if(invalidItem){
+            bidData = "No Result found on **"+itemName+"**.\nPlease check your search and try again.";
+        }else{
+            let selfBid = Math.floor(1 * itemPrice * ((playerCount - 1)/playerCount));
+            let marketBid = Math.floor((1 * itemPrice * ((playerCount - 1)/playerCount)) * (1 - .05 * 1));
 
-        let bidData = "**Max bid for the price of "+setCurrencyFormat(itemPrice)+"**\n"+
-                    "- Keeping the item for yourself\n"+setCurrencyFormat(selfBid)+"\n"+
-                    "- Sell to market\n"+setCurrencyFormat(marketBid);
+            bidData = "**Max bid for the price of "+setCurrencyFormat(itemPrice)+"**\n"+
+                      "- Keeping the item for yourself\n"+setCurrencyFormat(selfBid)+"\n"+
+                      "- Sell to market\n"+setCurrencyFormat(marketBid);
+        }
 
         msg.channel.stopTyping();
+        end = Date.now();
+        serveTime = (end-start)/1000+'s';
 
         let embedData = {
             "embed":{
@@ -76,7 +92,7 @@ module.exports = class RemoveCustomRoleCommand extends Command {
                 "color": 16766720,
                 "footer": {
                     "icon_url": "https://slate.silveress.ie/docs_bns/images/logo.png",
-                    "text": "Powered by Silveress's BnS API - Last update: "+dateformat(dataLastUpdate, "UTC:dd-mm-yy @ HH:MM")+" UTC"
+                    "text": "Powered by Silveress's BnS API - Served in "+serveTime
                 }
             }
         }
