02c99bf5bc5a41a32ca63011f2de69e143fbdb85
ln2r <scorch_62@live.com>
2020-03-25 05:37:14 +0700

    update(react-role): moved to events

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const dotenv = require('dotenv').config();
-
 const { CommandoClient } = require('discord.js-commando');
 const Twitter = require('twitter');
 const MongoClient = require('mongodb').MongoClient;
@@ -8,15 +6,23 @@ const path = require('path');
 const ontime = require('ontime');
 const dateformat = require('dateformat');
 
+const config = require('config.json');
+const services = require('./services/index.js');
+const events = require('./events/index.js');
+const utils = require('./utils/index.js');
+
 const {mongoGetData, sendResetNotification, mongoItemDataUpdate, sendBotReport, sendBotStats, getGuildSettings, getGlobalSettings } = require('./core');
 
+// Load config file
+services.loadConfig();
+
 // Maintenance mode
-const maintenanceMode = process.env.bot_maintenance_mode;
+const maintenanceMode = config.bot.maintenance;
 
 // Discord.js Commando scripts start here
 const clientDiscord = new CommandoClient({
-  commandPrefix: process.env.bot_default_prefix,
-  owner: process.env.bot_owner_id,
+  commandPrefix: config.bot.default_prefix,
+  owner: config.bot.author_id,
   disableEveryone: true,
   unknownCommandResponse: false,
 });
@@ -26,252 +32,86 @@ const reactionEvents = {
 	MESSAGE_REACTION_REMOVE: 'messageReactionRemove',
 };
 
-clientDiscord.login(process.env.discord_secret);
+clientDiscord.login(process.env.SOYUN_BOT_DISCORD_SECRET);
 
 clientDiscord.registry
-    .registerDefaultTypes()
-    .registerGroups([
-      ['system', 'Bot System'],
-      ['guild', 'Guild'],
-      ['bns', 'Blade and Soul'],
-      ['dev', 'Bot Dev'],
-    ])
-    .registerDefaultGroups()
-    .registerDefaultCommands()
-    .registerCommandsIn(path.join(__dirname, 'commands'));
+  .registerDefaultTypes()
+  .registerGroups([
+    ['system', 'Bot System'],
+    ['guild', 'Guild'],
+    ['bns', 'Blade and Soul'],
+    ['dev', 'Bot Dev'],
+  ])
+  .registerDefaultGroups()
+  .registerDefaultCommands()
+  .registerCommandsIn(path.join(__dirname, 'commands'));
 
 clientDiscord
-    .on('error', (error) => {
-      sendBotReport(error, 'system-soyun', 'error');
-      console.error(error);
-    })
-    .on('warn', (warn) => {
-      sendBotReport(warn, 'system-soyun', 'warning');
-      console.warn(warn);
-    })
-    // remove "//" below to enable debug log
-    // .on("debug", console.log)
-    .on('disconnect', () => {
-      console.warn('[soyun] [system] Disconnected!');
-    })
-    .on('reconnecting', () => {
-      console.warn('[soyun] [system] Reconnecting...');
-    })
-    .on('ready', async () => {
-      
-      let globalSettings = await getGuildSettings(0);
-      let botStatus = globalSettings.settings.status;
+  .on('error', (error) => {
+    sendBotReport(error, 'system-soyun', 'error');
+    console.error(error);
+  })
+  .on('warn', (warn) => {
+    sendBotReport(warn, 'system-soyun', 'warning');
+    console.warn(warn);
+  })
+  // remove "//" below to enable debug log
+  // .on("debug", console.log)
+  .on('disconnect', () => {
+    console.warn('[soyun] [system] Disconnected!');
+  })
+  .on('reconnecting', () => {
+    console.warn('[soyun] [system] Reconnecting...');
+  })
+  .on('ready', async () => {    
+    let globalSettings = await getGuildSettings(0);
+    let botStatus = globalSettings.settings.status;
 
-      if(maintenanceMode){
-        console.log("[soyun] [system] maintenance mode is enabled, only commands will run normally");
-        botStatus = {
-          game: {
-            name: "MAINTENANCE MODE",
-            type: "PLAYING"
-          },
-          status: "dnd",
-        };
+    if(maintenanceMode){
+      console.log("[soyun] [system] maintenance mode is enabled, only commands will run normally");
+      botStatus = {
+        game: {
+          name: "MAINTENANCE MODE",
+          type: "PLAYING"
+        },
+        status: "dnd",
       };
+    };
 
-      clientDiscord.user.setPresence(botStatus).catch((error) => {
-        console.error;
-        sendBotReport(error, 'onReady-soyun', 'error');
-      });
-
-      console.log('[soyun] [system] Logged in and ready');
-    })
-    .on('guildCreate', async guild => {
-      let guildSettingData = await getGuildSettings(guild.id);
-      
-      if(!guildSettingData){
-        clientDiscord.emit('commandPrefixChange', guild.id, process.env.bot_default_prefix);
-      };
-    })
-    .on('guildMemberAdd', async (member) => {
-      let guildSettingData = await getGuildSettings(member.guild.id);
-      let guildCommandPrefix = guildSettingData.settings.prefix;
-      
-      if(guildCommandPrefix === undefined || guildCommandPrefix === null){
-        guildCommandPrefix = "!";
-      }
-
-      if (guildSettingData) {
-        let memberGate = guildSettingData.settings.member_gate;
-        
-        // checking if the guild have the channel and the message set
-        if(memberGate){
-          if (memberGate.channel_id) {
-            member.guild.channels.find((ch) => ch.id === memberGate.channel_id).send(
-              'Hi <@'+member.user.id+'>! Welcome to ***'+member.guild.name+'***!\n\n'+
-
-              'Before I give you access to the rest of the server I need to know your character\'s name, to do that please use the following command with your information in it\n\n'+
-            
-              '`'+guildCommandPrefix+'join character name`\n'+
-              '**Example**:\n'+
-              '`'+guildCommandPrefix+'join jinsoyun `\n\n'+
-
-              'if you need any assistance you can mention or DM available admins, thank you ❤'
-            );
-          };
-        };
-      };
-    })
-    .on('commandRun', async () => {
-      if(maintenanceMode){
-        console.log("[soyun] [stats] command counter disabled");
-      }else{
-        await sendBotStats(Date.now());
-      };
-    })
-    .on('commandError', (error, command, message) => {
-      if(maintenanceMode){
-        console.log("[soyun] [error-report] error reporting disabled");
-      }else{
-        if(message.guild){
-          errorLocation = message.guild.name;
-          guildOwnerId = message.guild.ownerID;
-        }else{
-          errorLocation = "DIRECT_MESSAGE";
-          guildOwnerId = message.author.id;
-        }
-        // sending the error report to the database
-        sendBotReport(command.name+': '+command.message, error.name+'-'+errorLocation, 'error');
-        console.error('[soyun] ['+error.name+'] '+command.name+': '+command.message);
-
-        // dm bot owner for the error
-        let found = 0;
-        clientDiscord.guilds.map(function(guild) { // looking for the guild owner data (username and discriminator)
-          guild.members.map((member) => {
-            if (found === 0) {
-              if (member.id === guildOwnerId) {
-                found = 1;
-
-                for (let i=0; i < clientDiscord.owners.length; i++) {
-                  clientDiscord.owners[i].send(
-                      'Error Occured on `'+error.name+'`'+
-                      '\n__Details__:'+
-                      '\n**Time**: '+dateformat(Date.now(), 'dddd, dS mmmm yyyy, h:MM:ss TT')+
-                      '\n**Location**: '+errorLocation+
-                      '\n**Guild Owner**: '+member.user.username+'#'+member.user.discriminator+
-                      '\n**Content**: `'+message.content+'`'+
-                      '\n**Message**:\n'+command.name+': '+command.message
-                  ).then(
-                    function(message) {
-                      message.react('✅');
-                      message.react('❎');
-                    }
-                  ).catch((err) => {
-                    sendBotReport(err, 'errorDM-soyun', 'error');
-                  });
-                };
-              };
-            };
-          });
-        });
-      };
-    })
-    // event handling for reactions
-    .on('raw', async event => {
-      /**
-       * Original algorithm by Sam-DevZ
-       * https://github.com/Sam-DevZ/Discord-RoleReact
-       */
-
-      // check if the event have the reactions property
-      if(!reactionEvents.hasOwnProperty(event.t)) return;
-      const { d: data } = event;
-      const user = clientDiscord.users.get(data.user_id);
-
-      // check if it's the bot
-      if(data.user_id === clientDiscord.user.id) return;
-      
-      // get role data from db
-      let guildSettings = await getGuildSettings(data.guild_id);
-
-      if(guildSettings && guildSettings.length !== 0){
-        guildReactionRoleData = guildSettings.settings.react_role;
-    
-        if(guildReactionRoleData){
-          const channel = clientDiscord.channels.get(data.channel_id);
-          // checking channel and finding the message
-          let found = false;
-          let messageIndex;
-
-          for(let i=0; i<guildReactionRoleData.length; i++){
-            if(guildReactionRoleData[i].channel === data.channel_id && guildReactionRoleData[i].id === data.message_id){
-              messageIndex = i;
-              found = true;
-            };
-          };
-
-          if(found){
-            // checking the message
-            if(data.message_id === guildReactionRoleData[messageIndex].id){
-              let message;
-              let member;
-
-              // for error debugging, remove later
-              try{
-                message = await channel.fetchMessage(data.message_id);
-                member = message.guild.members.get(user.id);
-              }catch(err){
-                clientDiscord.owners[i].send(
-                  'Error Occured on `'+error.name+'`'+
-                    '\n__Details__:'+
-                    '\n**Time**: '+dateformat(Date.now(), 'dddd, dS mmmm yyyy, h:MM:ss TT')+
-                    '\n**Location**: '+data.guild_id+
-                    '\n**Content**: '+err+
-                    '\n**Message**:\n'+message
-                )
-              }
-
-              // checking the emoji and getting the index
-              if(guildReactionRoleData[messageIndex].reactions){
-                let emojiData;
-                if(data.emoji.id){
-                  emojiData = data.emoji.name+":"+data.emoji.id;
-                }else{
-                  emojiData = data.emoji.name;
-                };
-
-                // checking the emoji and getting the role id
-                let reactionFound = false;
-                let reactionIndex;
-                for(let i=0; i<guildReactionRoleData[messageIndex].reactions.length; i++){
-                  if(guildReactionRoleData[messageIndex].reactions[i].emoji === emojiData){
-                    reactionFound = true;
-                    reactionIndex = i;
-                  };
-                };
-
-                if(reactionFound){
-                  // checking if once
-                  if(guildReactionRoleData[messageIndex].reactions[reactionIndex].once){
-                    member.addRole(guildReactionRoleData[messageIndex].reactions[reactionIndex].role);
-                    // removing the reaction
-                    channel.fetchMessage(guildReactionRoleData[messageIndex].id).then(message => {
-                      let filtered = message.reactions.filter(reaction => reaction.emoji.name === data.emoji.name);
-
-                      filtered.forEach(reaction => reaction.remove(user.id));
-                    });
-                  }else{
-                    // check if the event add or remove
-                    if(event.t === "MESSAGE_REACTION_ADD"){
-                      member.addRole(guildReactionRoleData[messageIndex].reactions[reactionIndex].role);
-                    }else{
-                      member.removeRole(guildReactionRoleData[messageIndex].reactions[reactionIndex].role)
-                    };
-                  };
-                };
-              };                
-            };
-          };
-        };
-      };
+    clientDiscord.user.setPresence(botStatus).catch((error) => {
+      console.error;
+      sendBotReport(error, 'onReady-soyun', 'error');
     });
 
+    console.log('[soyun] [system] Logged in and ready');
+  })
+  .on('guildCreate', async guild => {
+    let guildSettingData = await getGuildSettings(guild.id);
+    
+    if(!guildSettingData){
+      clientDiscord.emit('commandPrefixChange', guild.id, process.env.bot_default_prefix);
+    };
+  })
+  .on('guildMemberAdd', async (member) => {
+    await events.onMemberAdd(member);
+  })
+  .on('commandRun', async () => {
+    if(maintenanceMode){
+      console.log("[soyun] [stats] command counter disabled");
+    }else{
+      await services.sendStats(Date.now());
+    };
+  })
+  .on('commandError', (error, command, message) => {
+    events.onCommandError(error, command, message);
+  })
+  // event handling for reactions
+  .on('raw', async event => {
+    services.reactionRole(event, clientDiscord);
+  });
+
 clientDiscord.setProvider(
-    MongoClient.connect(process.env.bot_mongodb_url, {useNewUrlParser: true, useUnifiedTopology: true}).then((client) => new MongoDBProvider(client, process.env.bot_mongodb_db_name))
+  MongoClient.connect(process.env.bot_mongodb_url, {useNewUrlParser: true, useUnifiedTopology: true}).then((client) => new MongoDBProvider(client, process.env.bot_mongodb_db_name))
 ).catch((error) => {
   console.error;
   sendBotReport(error, 'mongoDBProvider-soyun', 'error');
