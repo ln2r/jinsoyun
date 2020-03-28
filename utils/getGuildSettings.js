ec6529d5ba60ed79b4cbbdb4aea57b45d4a889bf
ln2r <scorch_62@live.com>
2020-03-29 04:45:48 +0700

    fix(func): missing async

--- utils/getGuildSettings.js
+++ utils/getGuildSettings.js
@@ -6,7 +6,7 @@ const utils = require('./index');
  * @param {Snowflake} guildId current guild id
  * @return {Object | null} guild setting data
  */
-module.exports = function(){
+module.exports = async function(){
   let guildData = await utils.fetchDB('configs', {guild: guildId});
     
   if(guildData.length !== 0){
