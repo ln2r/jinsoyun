7518bccc07ca096f98ddae7630406304146e336f
ln2r <scorch_62@live.com>
2020-03-31 07:26:42 +0700

    feat: restructured using new system

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-const { CommandoClient } = require('discord.js-commando');
+require('dotenv').config();
+
+const {CommandoClient} = require('discord.js-commando');
 const MongoClient = require('mongodb').MongoClient;
 const MongoDBProvider = require('./commando-provider-mongo');
 const path = require('path');
@@ -9,10 +11,8 @@ const services = require('./services/index.js');
 const events = require('./events/index.js');
 const utils = require('./utils/index.js');
 
-const { sendBotReport } = require('./core');
-
 // Load config file
-services.loadConfig();
+//services.loadConfig();
 
 // Discord.js Commando scripts start here
 const clientDiscord = new CommandoClient({
@@ -38,89 +38,98 @@ clientDiscord.registry
 
 clientDiscord
   .on('error', (error) => {
-    sendBotReport(error, 'system-soyun', 'error');
+    //TODO: winston integration
+    //sendBotReport(error, 'system-soyun', 'error');
     console.error(error);
   })
   .on('warn', (warn) => {
-    sendBotReport(warn, 'system-soyun', 'warning');
+    //TODO: winston integration
+    //sendBotReport(warn, 'system-soyun', 'warning');
     console.warn(warn);
   })
-  // remove "//" below to enable debug log
-  // .on("debug", console.log)
+// remove "//" below to enable debug log
+// .on("debug", console.log)
   .on('disconnect', () => {
+    //TODO: winston integration
     console.warn('[soyun] [system] Disconnected!');
   })
   .on('reconnecting', () => {
+    //TODO: winston integration
     console.warn('[soyun] [system] Reconnecting...');
   })
-  .on('ready', async () => {    
-    let globalSettings = await utils.getGuildSettings(0);
+  .on('ready', async () => {
+    const globalSettings = await utils.getGuildSettings(0);
     let botStatus = globalSettings.settings.status;
 
-    if(config.bot.maintenance){
-      console.log("[soyun] [system] maintenance mode is enabled, only commands will run normally");
+    if (config.bot.maintenance) {
+      //TODO: winston integration
+      console.log('[soyun] [system] maintenance mode is enabled, only commands will run normally');
       botStatus = {
         game: {
-          name: "MAINTENANCE MODE",
-          type: "PLAYING"
+          name: 'MAINTENANCE MODE',
+          type: 'PLAYING',
         },
-        status: "dnd",
+        status: 'dnd',
       };
-    };
+    }
 
     clientDiscord.user.setPresence(botStatus).catch((error) => {
+      //TODO: winston integration
       console.error;
-      sendBotReport(error, 'onReady-soyun', 'error');
+      //sendBotReport(error, 'onReady-soyun', 'error');
     });
-
+    //TODO: winston integration
     console.log('[soyun] [system] Logged in and ready');
   })
-  .on('guildCreate', async guild => {
-    let guildSettingData = await utils.getGuildSettings(guild.id);
-    
-    if(!guildSettingData){
+  .on('guildCreate', async (guild) => {
+    const guildSettingData = await utils.getGuildSettings(guild.id);
+
+    if (!guildSettingData) {
       clientDiscord.emit('commandPrefixChange', guild.id, process.env.bot_default_prefix);
-    };
+    }
   })
   .on('guildMemberAdd', async (member) => {
     await events.onMemberAdd(member);
   })
   .on('commandRun', async () => {
-    if(config.bot.maintenance){
-      console.log("[soyun] [stats] command counter disabled");
-    }else{
+    if (config.bot.maintenance) {
+      //TODO: winston integration
+      console.log('[soyun] [stats] command counter disabled');
+    } else {
       await services.sendStats(Date.now());
-    };
+    }
   })
   .on('commandError', (error, command, message) => {
-    events.onCommandError(error, command, message);
+    events.onCommandError(error, command, message, clientDiscord);
   })
-  // event handling for reactions
-  .on('raw', async event => {
+// event handling for reactions
+  .on('raw', async (event) => {
     services.reactionRole(event, clientDiscord);
   });
 
 clientDiscord.setProvider(
-  MongoClient.connect(process.env.bot_mongodb_url, {useNewUrlParser: true, useUnifiedTopology: true}).then((client) => new MongoDBProvider(client, process.env.bot_mongodb_db_name))
+  MongoClient.connect(process.env.SOYUN_BOT_DB_CONNECT_URL, {useNewUrlParser: true, useUnifiedTopology: true}).then((client) => new MongoDBProvider(client, process.env.SOYUN_BOT_DB_NAME))
 ).catch((error) => {
+  //TODO: winston integration
   console.error;
-  sendBotReport(error, 'mongoDBProvider-soyun', 'error');
+  //sendBotReport(error, 'mongoDBProvider-soyun', 'error');
 });
 
 // Discord.js Commando scripts end here
 
 // Twitter stream
-services.twitterStream();
+services.twitterStream(clientDiscord);
 
 // Automation
 // Quest reset notification
-if(config.bot.maintenance){
-  console.log("[soyun] [automation] system automation is disabled");
-}else{
+if (config.bot.maintenance) {
+  //TODO: winston integration
+  console.log('[soyun] [automation] system automation is disabled');
+} else {
   ontime({
     cycle: ['12:00:00'],
     utc: true,
-  }, async function(reset){
+  }, async function(reset) {
     await services.automationQuestReset(clientDiscord.guilds);
 
     reset.done();
@@ -131,7 +140,7 @@ if(config.bot.maintenance){
   ontime({
     cycle: ['00:50:00', '03:50:00', '06:50:00', '18:50:00', '21:50:00'],
     utc: true,
-  }, async function(koldrak){
+  }, async function(koldrak) {
     await services.automationKoldrak(clientDiscord.guilds);
 
     koldrak.done();
@@ -142,9 +151,9 @@ if(config.bot.maintenance){
   ontime({
     cycle: ['01:50:00'],
     utc: true,
-  }, async function(hunter){
+  }, async function(hunter) {
     await services.automationHunters(clientDiscord.guilds);
-    
+
     hunter.done();
     return;
   });
@@ -153,10 +162,10 @@ if(config.bot.maintenance){
   ontime({
     cycle: ['00:02'],
     utc: true,
-  }, function(items){
-    services.updateItemsMarket();
+  }, function(items) {
+    services.automationItemUpdate();
 
     items.done();
     return;
   });
-};
+}
