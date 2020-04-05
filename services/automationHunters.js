de57141433d01ac6fb0581ea87dd9b37c6a1de31
ln2r <scorch_62@live.com>
2020-04-05 09:32:16 +0700

    fix: added guild available check

--- services/automationHunters.js
+++ services/automationHunters.js
@@ -14,39 +14,41 @@ module.exports = async function(guildData) {
     return;
   }
 
-  guildData.guilds.cache.map(async function(guild) {
-  // getting guild setting data
-    const guildSettingData = await utils.getGuildSettings(guild.id);
+  guildData.cache.map(async function(guild) {
+    if(guild.available){
+      // getting guild setting data
+      const guildSettingData = await utils.getGuildSettings(guild.id);
 
-    let huntersChannel = '';
-    if (guildSettingData !== undefined) {
-      huntersChannel = guildSettingData.settings.hunters_refugee;
-    }
+      let huntersChannel = '';
+      if (guildSettingData !== undefined) {
+        huntersChannel = guildSettingData.settings.hunters_refugee;
+      }
 
-    let found = 0;
-    guild.channels.cache.map((ch) => {
-      if (found === 0) {
-        if (ch.id === huntersChannel && huntersChannel !== '' && huntersChannel !== 'disable') {
-          found = 1;
-          if (ch.permissionsFor(guildData.user).has('EMBED_LINKS', 'SEND_MESSAGES', 'VIEW_CHANNEL')) {
-            ch.send({
-              'embed': {
-                'color': 8388736,
-                'description': '**Hunter\'s Refugee** will be accessible in **10 Minutes**',
-                'author': {
-                  'name': 'Event Alert',
+      let found = 0;
+      guild.channels.cache.map((ch) => {
+        if (found === 0) {
+          if (ch.id === huntersChannel && huntersChannel !== '' && huntersChannel !== 'disable') {
+            found = 1;
+            if (ch.permissionsFor(guildData.user).has('EMBED_LINKS', 'SEND_MESSAGES', 'VIEW_CHANNEL')) {
+              ch.send({
+                'embed': {
+                  'color': 8388736,
+                  'description': '**Hunter\'s Refugee** will be accessible in **10 Minutes**',
+                  'author': {
+                    'name': 'Event Alert',
 
+                  },
+                  'footer': {
+                    'icon_url': 'https://cdn.discordapp.com/emojis/463569669584977932.png?v=1',
+                    'text': 'Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC',
+                  },
                 },
-                'footer': {
-                  'icon_url': 'https://cdn.discordapp.com/emojis/463569669584977932.png?v=1',
-                  'text': 'Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC',
-                },
-              },
-            });
+              });
+            }
           }
         }
-      }
-    });
+      });
+    }
   });
   sendLog('info', 'Hunter\'s Access', 'Access notification sent');
   return;
--- services/automationKoldrak.js
+++ services/automationKoldrak.js
@@ -14,39 +14,41 @@ module.exports = async function(guildData) {
     return;
   }
 
-  guildData.guilds.cache.map(async function(guild) {
-  // getting guild setting data
-    const guildSettingData = await utils.getGuildSettings(guild.id);
+  guildData.cache.map(async function(guild) {    
+    if(guild.available){
+      // getting guild setting data
+      const guildSettingData = await utils.getGuildSettings(guild.id);
 
-    let koldrakChannel = '';
-    if (guildSettingData !== undefined) {
-      koldrakChannel = guildSettingData.settings.koldrak;
-    }
+      let koldrakChannel = '';
+      if (guildSettingData !== undefined) {
+        koldrakChannel = guildSettingData.settings.koldrak;
+      }
 
-    let found = 0;
-    guild.channels.cache.map((ch) => {
-      if (found === 0) {
-        if (ch.id === koldrakChannel && koldrakChannel !== '' && koldrakChannel !== 'disable') {
-          found = 1;
-          if (ch.permissionsFor(guildData.user).has('EMBED_LINKS', 'SEND_MESSAGES', 'VIEW_CHANNEL')) {
-            ch.send({
-              'embed': {
-                'color': 8388736,
-                'description': '**Koldrak\'s Lair** will be accessible in **10 Minutes**',
-                'author': {
-                  'name': 'Epic Challenge Alert',
+      let found = 0;
+      guild.channels.cache.map((ch) => {
+        if (found === 0) {
+          if (ch.id === koldrakChannel && koldrakChannel !== '' && koldrakChannel !== 'disable') {
+            found = 1;
+            if (ch.permissionsFor(guildData.user).has('EMBED_LINKS', 'SEND_MESSAGES', 'VIEW_CHANNEL')) {
+              ch.send({
+                'embed': {
+                  'color': 8388736,
+                  'description': '**Koldrak\'s Lair** will be accessible in **10 Minutes**',
+                  'author': {
+                    'name': 'Epic Challenge Alert',
 
+                  },
+                  'footer': {
+                    'icon_url': 'https://cdn.discordapp.com/emojis/463569669584977932.png?v=1',
+                    'text': 'Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC',
+                  },
                 },
-                'footer': {
-                  'icon_url': 'https://cdn.discordapp.com/emojis/463569669584977932.png?v=1',
-                  'text': 'Generated at '+dateformat(Date.now(), 'UTC:dd-mm-yy @ HH:MM')+' UTC',
-                },
-              },
-            });
+              });
+            }
           }
         }
-      }
-    });
+      });
+    }
   });
   sendLog('info', 'Koldrak Announce', 'Koldrak access notification sent');
   return;
