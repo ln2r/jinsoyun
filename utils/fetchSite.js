537a21fb170a6a5d4514af61143ae0bff320b21b
ln2r <scorch_62@live.com>
2020-07-19 17:21:50 +0700

    feat(fetchSite): fixed timeout

--- utils/fetchSite.js
+++ utils/fetchSite.js
@@ -7,27 +7,28 @@ const sendLog = require('../services/sendLog');
  * @param {String} address site address
  * @return data fetched from site in JSON format
  * @example
- *  fetchSite("https://api.silveress.ie/bns/v3/items");
+ *  // Using the function locally
+ *  module.exports.getSiteData("https://api.silveress.ie/bns/v3/items");
+ *
+ *  // Using the function outside the file
+ *  core.getSiteData("https://api.silveress.ie/bns/v3/items");
  */
-module.exports = function(address) {
-  const FETCH_TIMEOUT = 10000;
-  let timedOut = false;
-
+module.exports = async function(address) {
   sendLog('debug', 'fetchSite', `Fetching data from ${address}...`);
-  const timeOut = setTimeout(function() {
-    timedOut = true;
-    sendLog('warn', 'fetchSite', `Request to '${address}' timed out.`);
-  }, FETCH_TIMEOUT);
-  
-  return fetch(address)
-    .then((res) => {
-      clearTimeout(timeOut);
-      if(!timedOut){
-        sendLog('debug', 'fetchSite', 'Fetched without problem.');
-        return res.json();
-      } else if(timedOut){
-        return {'status':'error', 'message':`Request to '${address}' timed out.`};
-      }      
+
+  function fetch_ (url){
+    return Promise.race([
+      fetch(url),
+      new Promise((_, reject) =>
+        setTimeout(() => reject(new Error('Request Timed Out.')), 10000)
+      )
+    ]);
+  }
+
+  return fetch_(address)
+    .then((res) =>{
+      sendLog('debug', 'fetchSite', 'Fetched without problem.');
+      return res;
     }).catch((err) => {
       sendLog('error', 'fetchSite', err);
       return {'status': 'error', err};
