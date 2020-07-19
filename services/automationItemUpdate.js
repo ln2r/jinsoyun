1486d3aba191477332c0ff32884865b88d71fb91
ln2r <scorch_62@live.com>
2020-07-19 07:51:01 +0700

    feat(automation-items): added debug logging

--- services/automationItemUpdate.js
+++ services/automationItemUpdate.js
@@ -11,6 +11,7 @@ const dbName = process.env.SOYUN_BOT_DB_NAME;
  * Used to update the item data with it"s market data
  */
 module.exports = async function() {
+  sendLog('info', 'Auto-Items', 'Running items update...');
   const start = Date.now();
 
   // getting api data
@@ -23,39 +24,42 @@ module.exports = async function() {
   if (itemsData.status === 'error' || marketData.status === 'error') {
     const end = Date.now();
     const updateTime = (end-start)/1000+'s';
-    await sendLog('warn', 'Items', `'Data update failed. (${updateTime})\n${itemsData}`);
+    sendLog('warn', 'Items', `'Data update failed. (${updateTime})\n${itemsData}`);
   } else {
+    sendLog('debug', 'Auto-Items', `Connecting to ${url}...`);
     MongoClient.connect(url, {useNewUrlParser: true, useUnifiedTopology: true}, async function(err, db) {
-      if (err) await sendLog('error', 'Auto-Items', err);
+      if (err) sendLog('error', 'Auto-Items', err);
       const dbo = db.db(dbName);
 
       dbo.listCollections({name: configs.collection.items})
         .next(async function(err, collinfo) {
-          if (err) await sendLog('error', 'Auto-Items', err);
+          if (err) sendLog('error', 'Auto-Items', err);
 
           // checking if the collection is exist or not
           if (collinfo) {
             dbo.collection(configs.collection.items).drop(async function(err) {
-              if (err) await sendLog('error', 'Auto-Items', err);
+              if (err) sendLog('error', 'Auto-Items', err);
             });
           }
 
           // updating items data
+          sendLog('debug', 'Auto-Items', `Updating ${configs.collection.items} data...`);
           dbo.collection(configs.collection.items).insertMany(itemsData, async function(err) {
-            if (err) await sendLog('error', 'Auto-Items', err);
+            if (err) sendLog('error', 'Auto-Items', err);
             db.close();
           });
 
           // updating market data
+          sendLog('debug', 'Auto-Items', `Updating ${configs.collection.market} data...`);
           dbo.collection(configs.collection.market).insertMany(marketData, async function(err) {
-            if (err) await sendLog('error', 'Auto-Items', err);
+            if (err) sendLog('error', 'Auto-Items', err);
             db.close();
           });
 
           const end = Date.now();
           const updateTime = (end-start)/1000+'s';
                     
-          await sendLog('info', 'Items', 'Data updated, time: '+updateTime);
+          sendLog('info', 'Auto-Items', 'Data updated, time: '+updateTime);
         });
     });
   }
