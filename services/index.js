def0092957df416bb78de9e1e02652145186f84d
ln2r <scorch_62@live.com>
2020-08-08 20:57:15 +0700

    feat(services): removed leftovers

--- services/index.js
+++ services/index.js
@@ -1,25 +1,13 @@
 const reactionRole = require('./reactionRole');
-const automationItemUpdate = require('./automationItemUpdate');
-const sendResetNotification = require('./sendResetNotification');
 const sendLog = require('./sendLog');
 const checkConfig = require('./checkConfig');
 
 const twitterStream = require('./twitterStream');
 
-const automationQuestReset = require('./automationQuestReset');
-const automationKoldrak = require('./automationKoldrak');
-const automationHunters = require('./automationHunters');
-
 module.exports = {
   reactionRole,
-  automationItemUpdate,
-  sendResetNotification,
   sendLog,
   checkConfig,
 
   twitterStream,
-
-  automationQuestReset,
-  automationKoldrak,
-  automationHunters,
 };
