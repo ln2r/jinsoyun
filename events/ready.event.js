a77517c6ff4a8b61fa8e0975ba6216ad2ffe0775
ln2r <scorch_62@live.com>
2022-01-31 10:27:20 +0700

    feat(discord-ready): add api connection check

--- events/ready.event.js
+++ events/ready.event.js
@@ -1,9 +1,14 @@
 import { discordClient } from "../configs/discord.config.js";
+import { getApi } from "../utils/getApi.util.js";
 
 export const event = {
   name: 'ready',
   once: true,
   async execute(client) {
+    // checking api connection
+    const api = await getApi(`${process.env.API_URL}/`, { cache: false });
+    if (api) console.log(`API connection established, all good!`);
+
     discordClient.user.setPresence({ activities: [{ name: 'Blade and Soul' }], status: 'online' });
 
     console.log(`Ready! Logged in as ${client.user.tag}`);
