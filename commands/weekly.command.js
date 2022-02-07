c1b9dec5a81685e0f69e14f100dcfa09a041780a
ln2r <scorch_62@live.com>
2022-02-07 09:01:49 +0700

    feat(weekly): re-enable

--- commands/weekly.command.js
+++ commands/weekly.command.js
@@ -8,23 +8,19 @@ export const command = {
           .setName('weekly')
           .setDescription('See current weekly challenges'),
   async execute(interaction) {
-    // const apiData = await getApi(`${process.env.API_URL}/challenges/weekly`);
+    const apiData = await getApi(`${process.env.API_URL}/challenges/weekly`);
 
-    // const embed = new MessageEmbed()
-    //   .setTitle(`Weekly Challenges`)
-    //   .setColor('GREEN')
-    //   .setDescription(getArrayContent(apiData.weekly))
-    //   .setFooter({text: `Weekly Challenges - Source: ${apiData.metadata.source}`})
-    //   .setTimestamp()
+    const embed = new MessageEmbed()
+      .setTitle(`Weekly Challenges`)
+      .setColor('GREEN')
+      .setDescription(getArrayContent(apiData.weekly))
+      .setFooter({text: `Weekly Challenges - Source: ${apiData.metadata.source}`})
+      .setTimestamp()
 
-    // await interaction.reply({ 
-    //     content: `Current weekly challenges`, 
-    //     components: [], 
-    //     embeds: [embed]
-    //   });
-
-    await interaction.reply({
-      content: `Disabled until better source found or hongmoon-archive challenges list back online.`
-    });
+    await interaction.reply({ 
+        content: `Current weekly challenges`, 
+        components: [], 
+        embeds: [embed]
+      });
   },
 }
