0076b1010e4e692065cef4033cba1a6f35ae4f98
ln2r <scorch_62@live.com>
2022-02-07 09:01:39 +0700

    feat(daily): re-enable

--- commands/daily.command.js
+++ commands/daily.command.js
@@ -15,23 +15,19 @@ export const command = {
     const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
     const query = (interaction.options.get('day'))?.value || days[new Date().getUTCDay()];
 
-    // const apiData = await getApi(`${process.env.API_URL}/challenges/${query.toLowerCase()}`);
+    const apiData = await getApi(`${process.env.API_URL}/challenges/${query.toLowerCase()}`);
 
-    // const embed = new MessageEmbed()
-    //   .setTitle(`Daily Challenges - ${query.replace(/^\w/, (c) => c.toUpperCase())}`)
-    //   .setColor('GREEN')
-    //   .setDescription(getArrayContent(apiData.daily))
-    //   .setFooter({text: `Daily Challenges - Source: ${apiData.metadata.source}`})
-    //   .setTimestamp()
+    const embed = new MessageEmbed()
+      .setTitle(`Daily Challenges - ${query.replace(/^\w/, (c) => c.toUpperCase())}`)
+      .setColor('GREEN')
+      .setDescription(getArrayContent(apiData.daily))
+      .setFooter({text: `Daily Challenges - Source: ${apiData.metadata.source}`})
+      .setTimestamp()
 
-    // await interaction.reply({ 
-    //     content: `Daily challenges for **${query.replace(/^\w/, (c) => c.toUpperCase())}**`, 
-    //     components: [], 
-    //     embeds: [embed]
-    //   });
-
-    await interaction.reply({
-      content: `Disabled until better source found or hongmoon-archive challenges list back online.`
-    })
+    await interaction.reply({ 
+        content: `Daily challenges for **${query.replace(/^\w/, (c) => c.toUpperCase())}**`, 
+        components: [], 
+        embeds: [embed]
+      });
   },
 }
